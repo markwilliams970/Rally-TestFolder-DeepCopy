fc07327855a8a18ad3a6fda35f4df2bb7699a2f1
markwilliams <markwilliams@markw-macbook-pro.local>
2013-02-20 16:25:08 -0700

    User-specific configuration variables

---
+++ my_vars.rb
@@ -0,0 +1,12 @@
+$my_base_url                     = "https://rally1.rallydev.com/slm"
+$my_username                     = "user@company.com"
+$my_password                     = "topsecret"
+$wsapi_version                   = "1.40"
+$my_workspace                    = "My Workspace"
+$my_project                      = "My Project 1"
+
+# Target project: (can be same as source)
+$target_project_name             = "My Project 2"
+
+# Source Test Folder
+$source_test_folder_formatted_id = "TF5"
